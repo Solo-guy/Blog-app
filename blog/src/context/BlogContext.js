02f404b65c340ebb29f957bab98982187a0eb29c
front end wizzard <101088369+Solo-guy@users.noreply.github.com>
2023-09-07 11:41:21 +0700

    update code

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,20 +1,22 @@
-import createDataContext from './createDataContext'
+import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
-    switch (action.type) {
-        case 'add_blogpost':
-            return [...state, { title: `Blog Post #${state.length +1}`}];
-            default:
-                return state;
-    }
+  switch (action.type) {
+    case 'add_blogpost':
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
+    default:
+      return state;
+  }
 };
 
-const addBlogPost = () => {
+const addBlogPost = dispatch => {
+  return () => {
     dispatch({ type: 'add_blogpost' });
+  };
 };
 
-export const { Context, Provider} = createDataContext(
-    blogReducer, 
-    { addBlogPost }, 
-    []
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost },
+  []
 );
